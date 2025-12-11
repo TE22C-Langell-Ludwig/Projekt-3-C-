89020353a2aa6ba1d02509670cefe9842a0553fd
TE22C-Ludwig-Langell <ludwig.langell@elev.ga.ntig.se>
2025-12-11 14:39:26 +0100

    More frontend stuff

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad0f08236dd7dfdf00fa620028cdca993846633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d34025cd7f5d9b850532862cf9f9bda9043139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
